d4310626d82b2eb426d1d3265f582b6e9c7a0ee5
Ana-Galvez <anamariagalvez01@gmail.com>
2025-08-11 23:33:44 -0300

    feat: title dinamico

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react"
+
 const App = () => {
+
+  const [count,setCount] = useState(0)
+
+  useEffect (()=>{
+    document.title = `Práctica useEffect ${count}`
+  },[count])
+
+
+
+  // const handleClick = () => {
+  //   setCount(count + 1)
+  // }
+
+
   return (
     <>
       <h1>useEffect</h1>
+      <button onClick=/*{handleClick}*/{()=>{setCount(count + 1)}}>Contador : {count}</button>
     </>
   )
 }
